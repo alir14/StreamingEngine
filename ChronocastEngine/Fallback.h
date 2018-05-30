8e422ee3f53d1aa4e8ef624278e5bc8d39b843b0
ali.rezazadeh <ali_r14@yahoo.com>
2018-05-30 08:05:42 +0800

    Remove Old structure

--- ChronocastEngine/Fallback.h
+++
@@ -1,8 +0,0 @@
-//#pragma once
-//class Fallback
-//{
-//public:
-//	Fallback();
-//	~Fallback();
-//};
-//
--- StreamControlEngine/FallBackModel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class FallBackModel
-{
-public:
-	FallBackModel();
-	~FallBackModel();
-};
-
